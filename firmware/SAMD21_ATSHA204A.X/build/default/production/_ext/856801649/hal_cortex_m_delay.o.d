ef9f9140d3839acc188607b93020d3f8d83bae06
FlyerInk <frenkie.wang@microchip.com>
2020-12-10 11:21:59 +0800

    修改忽略规则

--- firmware/SAMD21_ATSHA204A.X/build/default/production/_ext/856801649/hal_cortex_m_delay.o.d
+++
@@ -1,2 +0,0 @@
-build/default/production/_ext/856801649/hal_cortex_m_delay.o: \
- ..\src\config\default\library\cryptoauthlib\hal\hal_cortex_m_delay.c
--- firmware/SAMD21_ATSHA204A.X/build/default/production/_ext/868908375/atcacert_der.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/_ext/868908375/atcacert_der.o: \
- ..\src\config\default\library\cryptoauthlib\atcacert\atcacert_der.c \
- ..\src\config\default\library\cryptoauthlib\atcacert\atcacert_der.h \
- ..\src\config\default\library\cryptoauthlib\atcacert\atcacert.h
-
-..\src\config\default\library\cryptoauthlib\atcacert\atcacert_der.h:
-
-..\src\config\default\library\cryptoauthlib\atcacert\atcacert.h:
